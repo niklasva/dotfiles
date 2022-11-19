50649dfbbd851a82909e8a1afe18a8724cc175c9
Niklas Vågstedt <niklas@niklas.zone>
2022-11-19 11:53:38 +0100

    Emacs improvements

--- vim/.config/nvim/lua/plugins/lsp-config.lua
+++ vim/.config/nvim/lua/plugins/lsp-config.lua
@@ -3,6 +3,8 @@ local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 local function sem_token_attach(_)
@@ -23,39 +25,70 @@ local on_attach = function(client, bufnr)
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
     end
 
-
-
-    local function fun_format(diagnostic)
-        if diagnostic.severity == vim.diagnostic.severity.WARN then
-            return string.format("E: %s", diagnostic.message)
+    local function word_break(input_str, width)
+        words = {}
+        for w in input_str:gmatch("[^ ]+") do
+            table.insert(words, w)
         end
-        return diagnostic.message
-    end
+
+        local out_str = ""
+        local line = ""
+        for k, v in ipairs (words) do
+            if (string.len(line .. " " .. v)) <= width
+                -- and v ~= "(fix" and v ~= "(fixes"
+                then
+                    if (k ~= 1) then
+                        line = line .. " " .. v
+                    else
+                        line = v
+                    end
+                else
+                    out_str = out_str .. line .. "\n"
+                    line = v
+                end
+            end
+            out_str = out_str .. line
+            return out_str
+        end
+
 
     vim.lsp.buf.semantic_tokens_full()
     vim.cmd [[autocmd BufEnter,CursorHold,InsertLeave <buffer> lua vim.lsp.buf.semantic_tokens_full()]]
+
+    local float_width = 90
+    local function fun_format(diagnostic)
+
+        local severity_symbol = ""
+        if (diagnostic.severity == 1) then
+            severity_symbol = "[Error]"
+        elseif (diagnostic.severity == 2) then
+            severity_symbol = "[Warn]"
+        end
+
+        local text = word_break(severity_symbol .. " " .. diagnostic.message .. " [" .. diagnostic.code .. "]", float_width)
+        return text
+    end
+
     vim.diagnostic.config({
-        signs = true,
+        severity_sort = true,
+        signs = false,
         underline = true,
         update_in_insert = false,
         virtual_text = false,
-        severity_sort = true,
+
         float = {
-            --prefix = ' ',
-            border = 'single',
+            border = 'rounded',
             close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
             focusable = false,
             format = fun_format,
-
             header = { '', 'Normal' },
             relative = 'win',
-            scope = 'line',
-            source = 'always',
+            --scope = 'line',
             style = 'minimal',
-            width = 60,
+            ----  width = float_width + 5
         },
     })
-    vim.o.updatetime = 150
+    vim.o.updatetime = 500
 
     vim.api.nvim_create_autocmd("CursorHold", {
         buffer = bufnr,
@@ -64,6 +97,7 @@ local on_attach = function(client, bufnr)
             vim.diagnostic.open_float(nil, opts)
         end
     })
+
 end
 -- end on_attach --
 
