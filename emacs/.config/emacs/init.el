f00dba5e2dffae8056b740b3cfab43309407e606
Niklas <niklas@niklas.zone>
2023-10-01 16:19:35 +0200

    emacs: byte-compile config on exit

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -19,6 +19,6 @@
 
 (if (file-exists-p "~/.config/emacs/config.elc")
   (load "~/.config/emacs/config.elc")
-  (if (file-exists-p "~/.config/emacs/config.elc")
-    (org-babel-load-file (expand-file-name "config.el" user-emacs-directory))
+  (if (file-exists-p "~/.config/emacs/config.el")
+    (load (expand-file-name "config.el" user-emacs-directory))
     (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
