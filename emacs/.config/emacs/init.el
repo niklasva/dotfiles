39649ed2bb4134c0cf5e6b2c1dea55cda950ac1f
Niklas Vågstedt <niklas@parakey.co>
2024-01-08 11:06:58 +0100

    emacs: disable config-file compilation

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -17,8 +17,10 @@
 (straight-use-package 'use-package)
 
 
-(if (file-exists-p "~/.config/emacs/config.elc")
-  (load "~/.config/emacs/config.elc")
-  (if (file-exists-p "~/.config/emacs/config.el")
-    (load (expand-file-name "config.el" user-emacs-directory))
-    (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
+;; (if (file-exists-p "~/.config/emacs/config.elc")
+;;   (load "~/.config/emacs/config.elc")
+;;   (if (file-exists-p "~/.config/emacs/config.el")
+;;     (load (expand-file-name "config.el" user-emacs-directory))
+;;     (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
+
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
