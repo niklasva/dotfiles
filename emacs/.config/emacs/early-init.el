4fb5ebb23b4c7ca1d37052dc4f0567fc3d24c540
Niklas Vågstedt <niklas@parakey.co>
2024-01-12 10:36:06 +0100

    emacs: change theming

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,29 +1,26 @@
 (setq package-enable-at-startup nil)
 (setq native-comp-speed -1)
 (setq default-frame-alist
-      (append (list
-               '(min-height              . 1)
-               '(height                  . 45)
-               '(min-width               . 1)
-               '(width                   . 155)
-               '(vertical-scroll-bars    . nil)
-               '(internal-border-width   . 0)
-               ;; '(left-fringe             . 7)
-               ;; '(right-fringe            . 7)
-               '(tool-bar-lines          . 0)
-               ;;'(undecorated-round       . t)
-               '(ns-transparent-titlebar . t)
-               '(ns-appearance           . dark)
-               '(font                    . "Liga Mononoki 16"))))
+    (append (list '(min-height              . 1)
+                  '(height                  . 45)
+                  '(min-width               . 1)
+                  '(width                   . 155)
+                  '(internal-border-width   . 24)
+                  '(vertical-scroll-bars    . nil)
+                  '(tool-bar-lines          . 0)
+                  '(ns-transparent-titlebar . t)
+                  '(ns-appearance           . dark)
+                  '(font                    . "Iosevka Comfy 17"))))
 
-(set-face-attribute 'default nil :font "Liga Mononoki 16")
+(set-face-attribute 'default nil :font "Iosevka Comfy 17" :weight 'regular)
 
 (setq ns-antialias-text t)
 (setq ns-use-thin-smoothing nil)
+(setq ns-use-native-fullscreen nil)
 (setq custom-safe-themes t)
 
-;; (ignore-errors
-;;   (load-theme 'wheatgrass))
+;;(ignore-errors
+;;    (load-theme 'wheatgrass))
 
 (setq lexical-binding t)
 (require 'use-package-core)
