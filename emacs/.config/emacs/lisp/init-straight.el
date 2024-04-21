8d6e16e669a18472050ef06dc18027cd57f2fe9c
Niklas <niklas@niklas.zone>
2024-04-21 22:53:20 +0200

    emacs: early init

--- emacs/.config/emacs/lisp/init-straight.el
+++ emacs/.config/emacs/lisp/init-straight.el
@@ -1,18 +1,16 @@
-(setq vc-follow-symlinks t)
-
 (setq package-enable-at-startup nil)
 (defvar bootstrap-version)
+(require 'use-package-core)
 (let ((bootstrap-file
-        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-      (url-retrieve-synchronously
-        "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-        'silent 'inhibit-cookies)
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-  (straight-use-package 'org)
+(straight-use-package 'org)
 (straight-use-package 'use-package)
-
