78e6bfef6e9d819f0329caeb5b74235652f43ea4
Niklas <niklas@niklas.zone>
2024-04-21 22:53:07 +0200

    emacs: early init window

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,36 +1,33 @@
 (setq vc-follow-symlinks t)
-
 (setq native-comp-speed -1)
+(setq lexical-binding t)
+
 (setq default-frame-alist
-    (append (list '(min-height              . 1)
-                  '(height                  . 45)
-                  '(min-width               . 1)
-                  '(width                   . 155)
-                  '(internal-border-width   . 0)
-                  '(vertical-scroll-bars    . nil)
-                  '(tool-bar-lines          . 0)
-                  '(ns-transparent-titlebar . t)
-                  '(ns-appearance           . dark)
-                  '(font                    . "Iosevka Comfy 15"))))
+      (append (list '(min-height              . 1)
+                    '(height                  . 45)
+                    '(min-width               . 1)
+                    '(width                   . 155)
+                    '(internal-border-width   . 0)
+                    '(vertical-scroll-bars    . nil)
+                    '(tool-bar-lines          . 0)
+                    '(ns-transparent-titlebar . t)
+                    '(ns-appearance           . dark)
+                    '(font                    . "Iosevka Comfy 15"))))
 
-(set-face-attribute 'default nil :font "Iosevka Comfy 15" :height 160 :weight 'regular)
+(load (expand-file-name "lisp/init-straight.el" user-emacs-directory))
+(load (expand-file-name "lisp/theme-packages.el" user-emacs-directory))
 
-(setq ns-antialias-text t)
-(setq ns-use-thin-smoothing t)
-(setq ns-use-native-fullscreen t)
-(setq custom-safe-themes t)
-(setq frame-title-format "")
+(setq frame-resize-pixelwise t
+      frame-title-format ""
+      ns-antialias-text t
+      ns-use-proxy-icon nil
+      ns-use-thin-smoothing t)
+
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode nil)
 (menu-bar-mode 1)
 (set-fringe-mode 1)
 
-(setq lexical-binding t)
-(require 'use-package-core)
-
-(load (expand-file-name "lisp/init-straight.el" user-emacs-directory))
-(load (expand-file-name "lisp/theme-packages.el" user-emacs-directory))
-
-(setq niva/theme 'ef-tritanopia-dark)
+(setq niva/theme 'doom-dark+)
 (load-theme niva/theme)
