bbb798f925455974d71e0d54c7cfc678ae7e4a5a
Niklas <niklas@niklas.zone>
2024-04-28 22:43:24 +0200

    emacs: clean up init.el

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,7 +1,1 @@
-;; (if (file-exists-p "~/.config/emacs/config.elc")
-;;   (load "~/.config/emacs/config.elc")
-;;   (if (file-exists-p "~/.config/emacs/config.el")
-;;     (load (expand-file-name "config.el" user-emacs-directory))
-;;     (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
-
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
