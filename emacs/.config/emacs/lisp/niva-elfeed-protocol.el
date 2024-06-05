9d80a86d5921163d4c3a246d962b45ea1d9394e5
Niklas Vågstedt <niklas@parakey.co>
2024-06-05 14:07:46 +0200

    emacs: begin moving elfeed functions to modules

---
+++ emacs/.config/emacs/lisp/niva-elfeed-protocol.el
@@ -0,0 +1,139 @@
+;;; niva-elfeed-protocol.el --- Extensions to elfeed-protocol -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(advice-add 'elfeed-protocol-fever--parse-entries :override
+            (defun niva/elfeed-protocol-fever--parse-entries (host-url items &optional mark-state update-action callback)
+              "Parse the entries JSON buffer and fill results to elfeed db.
+HOST-URL is the host name of Fever server.  ITEMS is the result JSON items by
+http request.  If MARK-STATE is nil, then just not update :last-entry-id or
+:first-entry-id values.  UPDATE-ACTION could be update, update-older or
+update-star.  If CALLBACK is not nil, will call it with the result entries as
+argument.  Return parsed entries."
+              (if (> (hash-table-count elfeed-protocol-fever-feeds) 0)
+                  (let* ((proto-id (elfeed-protocol-fever-id host-url))
+                         (entry-mark (elfeed-protocol-fever-get-update-mark proto-id update-action))
+                         (min-entry-id -1)
+                         (max-entry-id -1)
+                         (first-entry-id -1)
+                         (unread-num 0)
+                         (starred-num 0)
+                         (begin-time (time-to-seconds))
+                         entries)
+                    (elfeed-log 'debug "elfeed-protocol-fever: %s, parsing entries, entry-mark: %d" update-action entry-mark)
+                    (elfeed-log 'info "BANANA BOAT")
+                    (setq entries
+                          (cl-loop for item across items collect
+                                   (pcase-let* (((map id ('url entry-url) title
+                                                      author ('created_on_time pub-date) ('html body)
+                                                      ('feed_id feed-id))
+                                                 item)
+
+                                                (pub-date (if (string-equal author "Richard Stallman")
+                                                              (- pub-date 21600)
+                                                            pub-date))
+
+
+                                                (id (if (stringp id) (string-to-number id) id))
+                                                (feed-id (if (stringp feed-id) (string-to-number feed-id) feed-id))
+                                                (guid-hash (elfeed-generate-id (format "%s%s%s%s" title entry-url pub-date body)))
+                                                (feed-url
+                                                 (if (null feed-id)
+                                                     ""
+                                                   (elfeed-protocol-fever--get-subfeed-url host-url feed-id)))
+                                                (unread (eq (map-elt item 'is_read) 0))
+                                                (starred (eq (map-elt item 'is_saved) 1))
+
+                                                (namespace (elfeed-url-to-namespace feed-url))
+                                                (full-id (cons namespace (elfeed-cleanup guid-hash)))
+                                                (original (elfeed-db-get-entry full-id))
+                                                (original-date (and original
+                                                                    (elfeed-entry-date original)))
+                                                (category-name (when elfeed-protocol-fever-fetch-category-as-tag
+                                                                 (elfeed-protocol-fever--get-category-name
+                                                                  host-url
+                                                                  (elfeed-protocol-fever--get-subfeed-category-id host-url feed-id))))
+                                                (autotags (elfeed-protocol-feed-autotags proto-id feed-url))
+                                                (fixtags (elfeed-normalize-tags
+                                                          autotags elfeed-initial-tags))
+                                                (tags (progn
+                                                        (unless unread
+                                                          (setq fixtags (delete 'unread fixtags)))
+                                                        (when starred
+                                                          (push elfeed-protocol-fever-star-tag fixtags))
+                                                        (when category-name
+                                                          (push (intern category-name) fixtags))
+                                                        fixtags))
+                                                (db-entry (elfeed-entry--create
+                                                           :title (elfeed-cleanup title)
+                                                           :id full-id
+                                                           :feed-id (elfeed-protocol-format-subfeed-id
+                                                                     proto-id feed-url)
+                                                           :link (elfeed-cleanup entry-url)
+                                                           :tags tags
+                                                           :date (elfeed-new-date-for-entry
+                                                                  original-date pub-date)
+                                                           :content body
+                                                           :content-type 'html
+                                                           :meta `(,@(elfeed-protocol-build-meta-author author)
+                                                                   ,@(list :protocol-id proto-id
+                                                                           :id id
+                                                                           :guid-hash guid-hash
+                                                                           :feed-id feed-id)))))
+                                     (when unread (setq unread-num (1+ unread-num)))
+                                     (when starred (setq starred-num (1+ starred-num)))
+
+                                     ;; force override unread and star tags without repeat sync operation
+                                     (when original
+                                       (if unread (elfeed-tag-1 original 'unread)
+                                         (elfeed-untag-1 original 'unread))
+                                       (if starred (elfeed-tag-1 original elfeed-protocol-fever-star-tag)
+                                         (elfeed-untag-1 original elfeed-protocol-fever-star-tag)))
+
+                                     (when (> id max-entry-id)
+                                       (setq max-entry-id id))
+                                     (if (< min-entry-id 0)
+                                         (setq min-entry-id id)
+                                       (when (< id min-entry-id)
+                                         (setq min-entry-id id)))
+
+                                     (dolist (hook elfeed-new-entry-parse-hook)
+                                       (run-hook-with-args hook :fever item db-entry))
+                                     db-entry)))
+                    (elfeed-db-add entries)
+                    (when callback (funcall callback entries))
+
+                    ;; update last entry skip count
+                    (when mark-state
+                      (if (>= entry-mark 0)
+                          ;; update entry mark
+                          (cond
+                           ((eq update-action 'update)
+                            (elfeed-protocol-fever-set-update-mark
+                             proto-id update-action (max entry-mark max-entry-id)))
+                           ((eq update-action 'update-older)
+                            (let* ((id (max 1 (- entry-mark elfeed-protocol-fever-maxsize))))
+                              (elfeed-protocol-fever-set-update-mark
+                               proto-id update-action id))))
+                        ;; init entry mark
+                        (setq first-entry-id (max 1 max-entry-id))
+                        (cond
+                         ((eq update-action 'update)
+                          (elfeed-protocol-fever-set-update-mark proto-id update-action first-entry-id)
+                          ;; set :first-entry-id same with :last-entry-id
+                          (elfeed-protocol-fever-set-update-mark proto-id 'update-older first-entry-id))
+                         ((eq update-action 'update-older)
+                          (elfeed-protocol-fever-set-update-mark proto-id update-action first-entry-id)))))
+
+                    (elfeed-log 'debug "elfeed-protocol-fever: %s, parsed %d entries(%d unread, %d starred, min-entry-id %d, max-entry-id %d) with %fs, entry-mark: %d"
+                                update-action (length entries) unread-num starred-num min-entry-id max-entry-id
+                                (- (time-to-seconds) begin-time)
+                                (elfeed-protocol-fever-get-update-mark proto-id update-action))
+                    entries)
+                (progn
+                  (elfeed-log 'error "elfeed-protocol-fever: elfeed-protocol-fever-feeds is nil, please call elfeed-protocol-fever--update-feed-list first")
+                  nil)))
+            )
+
+(provide 'niva-elfeed-protocol)
+;;; niva-elfeed-protocol.el ends here
---
+++ emacs/.config/emacs/lisp/niva-elfeed.el
@@ -0,0 +1,105 @@
+;;; niva-elfeed.el --- Extensions to elfeed -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(defun niva/fixed-length (input-string length)
+  "Ensure that INPUT-STRING is exactly LENGTH characters long, padding with spaces if necessary."
+  (let ((len (length input-string)))
+    (if (>= len length)
+        (substring input-string 0 length)
+      (concat input-string (make-string (- length len) ? )))))
+
+(defun elfeed-search--faces-1 (tags)
+  "Return all the faces that apply to an entry with TAGS."
+  (nconc (cl-loop for (tag . faces) in elfeed-search-face-alist-1
+                  when (memq tag tags)
+                  append faces)
+         (list 'elfeed-search-title-face)))
+
+(setq elfeed-search-face-alist-1
+      '((unread elfeed-search-unread-title-face)))
+
+(defun elfeed-search--faces-2 (tags)
+  "Return all the faces that apply to an entry with TAGS."
+  (nconc (cl-loop for (tag . faces) in elfeed-search-face-alist-2
+                  when (memq tag tags)
+                  append faces)
+         (list 'elfeed-search-title-face)))
+
+(setq elfeed-search-face-alist-2
+      '((unread elfeed-search-feed-face)))
+
+(defun elfeed-search--faces-3 (tags)
+  "Return all the faces that apply to an entry with TAGS."
+  (nconc (cl-loop for (tag . faces) in elfeed-search-face-alist-3
+                  when (memq tag tags)
+                  append faces)
+         (list 'elfeed-search-title-face)))
+
+(setq elfeed-search-face-alist-3
+      '((unread elfeed-search-tag-face)))
+
+(defun elfeed-search--faces-4 (tags)
+  "Return all the faces that apply to an entry with TAGS."
+  (nconc (cl-loop for (tag . faces) in elfeed-search-face-alist-4
+                  when (memq tag tags)
+                  append faces)
+         (list 'elfeed-search-title-face)))
+
+(setq elfeed-search-face-alist-4
+      '((unread elfeed-search-date-face)))
+
+(setq widest-feed-title 0)
+(defun niva/elfeed-search-print-entry (entry)
+  (let* ((feed (elfeed-entry-feed entry))
+         (feed-title (when feed (or (elfeed-meta feed :title) (elfeed-feed-title feed))))
+         (star (if (member "star" (mapcar #'symbol-name (elfeed-entry-tags entry))) "s" " "))
+         (tags (delete "unread" (delete "star" (mapcar #'symbol-name (elfeed-entry-tags entry)))))
+         (tags-str "%-12s ")
+         (date (format "%-12s " (relative-date  (elfeed-entry-date entry))))
+         (title (or (elfeed-meta entry :title) (elfeed-entry-title entry) ""))
+         (title-faces      (elfeed-search--faces-1 (elfeed-entry-tags entry)))
+         (feed-title-faces (elfeed-search--faces-2 (elfeed-entry-tags entry)))
+         (feed-tag-faces   (elfeed-search--faces-3 (elfeed-entry-tags entry)))
+         (feed-date-faces  (elfeed-search--faces-4 (elfeed-entry-tags entry)))
+         (formatted-date (propertize (format "%s" date) 'face feed-date-faces))
+         (formatted-tags (propertize (format tags-str (mapconcat 'identity tags " ")) 'face feed-tag-faces))
+         (formatted-feed-title (propertize feed-title 'face feed-title-faces))
+         (title-width-wide (- (window-width) (string-width formatted-date) (string-width formatted-tags) 25 5))
+         (title-width (- (window-width) (string-width formatted-date)))
+         (formatted-title-wide (propertize (elfeed-format-column title title-width-wide :left) 'face title-faces))
+         (formatted-title (propertize (elfeed-format-column title title-width :left) 'face title-faces))
+         (end-of-window (format (format "%%%ds" (- (window-width) (string-width formatted-feed-title) (string-width formatted-tags))) "")))
+
+    (if (< widest-feed-title (string-width formatted-feed-title))
+        (setq widest-feed-title (string-width formatted-feed-title)))
+    (setq feed-padding (format "%%-%ds " widest-feed-title))
+
+    (if (> (window-width) 100)
+        (mapc #'insert (list
+                        formatted-date
+                        (niva/fixed-length formatted-feed-title 25)
+                        formatted-tags
+                        formatted-title-wide
+                        ))
+      (mapc #'insert (list
+                      formatted-date
+                      formatted-feed-title
+                      end-of-window
+                      formatted-tags
+                      formatted-title
+                      )))))
+
+
+(setq elfeed-search-print-entry-function #'niva/elfeed-search-print-entry)
+
+(with-eval-after-load 'elfeed
+  (when (eq niva/theme 'default)
+    (set-face-attribute 'elfeed-search-date-face         nil :foreground nil :bold nil :inherit 'org-agenda-date)
+    (set-face-attribute 'elfeed-search-feed-face         nil :foreground nil :bold nil :inherit 'gnus-header-from)
+    (set-face-attribute 'elfeed-search-tag-face          nil :foreground nil :bold nil :inherit 'gnus-header-newsgroups)
+    (set-face-attribute 'elfeed-search-title-face        nil :foreground nil :bold nil :inherit 'shadow)
+    (set-face-attribute 'elfeed-search-unread-title-face nil :foreground nil :bold nil :inherit 'gnus-header-subject)))
+
+(provide 'niva-elfeed)
+;;; niva-elfeed.el ends here
