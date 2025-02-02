fbc3d7d369fabce97190f169f3b7bfbe7fc39ee6
Niklas <niklas@niklas.zone>
2025-02-02 14:46:21 +0100

    emacs: new init

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,1 +1,4 @@
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(setq use-new-config nil) 
+(if use-new-config
+    (org-babel-load-file (expand-file-name "new-init.el" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
---
+++ emacs/.config/emacs/new-init.el
@@ -0,0 +1,35 @@
+;;;;;;;;;;;;; new init ;;;;;;;;;;;
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+
+
+(defvar import-file (expand-file-name "private/config.el" user-emacs-directory))
+(when (file-readable-p import-file) (load import-file))
+
+(setq-default show-trailing-whitespace t)
+
+(fido-vertical-mode)
+(recentf-mode)
+
+(use-package gptel
+  :diminish gptel-mode
+  :defer t
+  :straight (gptel :host github :repo "karthink/gptel" branch "master")
+  :config
+  (setq gptel-default-mode #'org-mode
+        gptel-prompt-prefix-alist '((org-mode . "> "))))
+
+(use-package exec-path-from-shell :straight t :init (exec-path-from-shell-initialize))
+(use-package consult :straight t :bind (("C-x b" . consult-buffer)))
+(use-package orderless :straight t)
+
+(global-completion-preview-mode)
+(setq completion-styles '(orderless basic)
+      completion-auto-help 'visible ;; Display *Completions* upon first request
+      completions-format 'one-column ;; Use only one column
+      completions-sort 'historical ;; Order based on minibuffer history
+      completions-max-height 20 ;; Limit completions to 15 (completions start at line 5)
+      completion-category-defaults nil
+      completion-category-overrides '((file (styles . (partial-completion)))))
+
+(define-key minibuffer-local-completion-map (kbd "SPC") 'self-insert-command)
