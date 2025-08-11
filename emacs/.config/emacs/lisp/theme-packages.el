8726bf7e590bc16cc6787a6569cf3e5bd66597ee
Niklas Vågstedt <niklas@parakey.co>
2025-08-11 21:04:54 +0200

    emacs: themes

--- emacs/.config/emacs/lisp/theme-packages.el
+++ emacs/.config/emacs/lisp/theme-packages.el
@@ -45,20 +45,16 @@
   :config
   (setq-default acme-theme-black-fg t))
 
-(use-package ef-themes
-  :straight (:host github :repo "protesilaos/ef-themes")
-  :defer t)
-
-(use-package doric-themes
-  :straight (:host github :repo "protesilaos/doric-themes")
-  :defer t)
-
-(use-package os1-theme
-  :straight (:type git :host github :repo "sashimacs/os1-theme")
-  :defer t)
-
-(use-package colorless-themes
-  :straight (:host github :repo "lthms/colorless-themes.el" :files ("colorless-themes.el" "*.el")))
+(use-package ef-themes          :straight (:host github :repo "protesilaos/ef-themes")                :defer t)
+(use-package orangey-bits-theme :straight (:host github :repo "emacsfodder/emacs-theme-orangey-bits") :defer t)
+(use-package cyanometric-theme  :straight (:host github :repo "emacsfodder/emacs-theme-cyanometric")  :defer t)
+(use-package vegetative-theme   :straight (:host github :repo "emacsfodder/emacs-theme-vegetative")   :defer t)
+(use-package dark-krystal-theme :straight (:host github :repo "emacsfodder/emacs-dark-krystal-theme") :defer t)
+(use-package doric-themes       :straight (:host github :repo "protesilaos/doric-themes")             :defer t)
+(use-package os1-theme          :straight (:host github :repo "sashimacs/os1-theme")                  :defer t)
+(use-package south              :straight (:host github :repo "sophiebosio/south")                    :defer t)
+(use-package mruber-darker      :straight (:host github :repo "monteiroliveira/mruber-darker-theme")  :defer t)
+(use-package colorless-themes   :straight (:host github :repo "lthms/colorless-themes.el"             :files ("colorless-themes.el" "*.el")))
 
 (use-package alect-themes          :straight t :defer t)
 (use-package almost-mono-themes    :straight t :defer t)
@@ -72,16 +68,26 @@
 (use-package parchment-theme       :straight t :defer t)
 (use-package professional-theme    :straight t :defer t)
 (use-package standard-themes       :straight t :defer t)
+(use-package tomorrow-night-deepblue-theme :straight t :defer t)
 
-(use-package solarized-theme :straight t
+(use-package solarized-theme
+  :straight t
+  :defer t
+  :custom
+  (solarized-scale-org-headlines nil)
+  (solarized-scale-outline-headlines nil)
+  (solarized-scale-markdown-headlines nil)
+  (solarized-use-less-bold t)
+  (solarized-highlight-numbers t)
+  (solarized-high-contrast-mode-line nil))
+
+(use-package lambda-themes
+  :straight (:host github :repo "lambda-emacs/lambda-themes")
   :defer t
   :config
-  :config
-  (setq-default solarized-scale-org-headlines nil
-                solarized-scale-outline-headlines nil
-                solarized-scale-markdown-headlines nil
-                solarized-high-contrast-mode-line nil))
-
+  (setq-default lambda-themes-set-italic-comments t)
+  (setq-default lambda-themes-set-italic-keywords t)
+  (setq-default lambda-themes-set-variable-pitch t))
 
 (use-package modus-themes
   :straight t
@@ -113,10 +119,12 @@
   (interactive)
   (ignore-errors
     (when (or (null custom-enabled-themes) (niva/theme-is-active "bing-bong-light"))
-      (custom-set-faces '(org-block                   ((t (:inherit 'default))))
-                        '(org-block-begin-line        ((t (:inherit 'org-block :extend t :overline t   :underline nil))))
-                        '(org-block-end-line          ((t (:inherit 'org-block :extend t :overline nil :underline t))))
-                        '(eglot-highlight-symbol-face ((t (:underline nil :bold t))))))
+      (custom-set-faces
+       '(default                   ((t (:foreground "#000"))))
+       '(org-block                   ((t (:inherit 'default))))
+       '(org-block-begin-line        ((t (:inherit 'org-block :extend t :overline t   :underline nil))))
+       '(org-block-end-line          ((t (:inherit 'org-block :extend t :overline nil :underline t))))
+       '(eglot-highlight-symbol-face ((t (:underline nil :bold t))))))
 
     (when (niva/theme-is-active "doric")
       (custom-set-faces '(font-lock-type-face          ((t :inherit unspecified :bold t)))
@@ -125,13 +133,6 @@
 
     (when (niva/theme-is-active "doom-tomorrow-day")
       (custom-set-faces '(font-lock-number-face ((t (:foreground unspecified :inherit 'font-lock-builtin-face))))))
-
-    (when (niva/theme-is-active "doom-tomorrow-night")
-      (custom-set-faces '(font-lock-number-face       ((t (:foreground unspecified :inherit 'font-lock-builtin-face))))
-                        '(font-lock-variable-use-face ((t (:foreground unspecified :inherit 'default))))
-                        '(font-lock-constant-face     ((t (:foreground unspecified :inherit 'font-lock-number-face))))
-                        '(warning                     ((t (:foreground unspecified :inherit 'font-lock-builtin-face))))
-                        '(font-lock-type-face         ((t (:foreground unspecified :inherit 'font-lock-builtin-face))))))
 
     (when (niva/theme-is-active "less")
       (custom-set-faces '(elfeed-search-unread-title-face ((t :inherit 'default :foreground 'unspecified)))
@@ -150,9 +151,16 @@
                         '(link                 ((t (:inherit 'font-lock-comment-face))))))
 
     (when (niva/theme-is-active "acme")
+      (custom-set-faces '(font-lock-variable-name-face ((t (:weight unspecified)))))
+      (custom-set-faces '(font-lock-function-name-face ((t (:weight unspecified)))))
+      (custom-set-faces '(font-lock-constant-face ((t (:weight unspecified)))))
+      (custom-set-faces '(font-lock-type-face ((t (:weight unspecified)))))
+      (custom-set-faces '(font-lock-keyword-face ((t (:weight unspecified)))))
       (custom-set-faces '(org-block ((t (:background "#FFFFDC"))))
                         '(org-block-begin-line ((t (:extend t :overline t :underline nil :background "#FFFFDC"))))
-                        '(org-block-end-line   ((t (:extend t :overline nil :underline t :background "#FFFFDC"))))))
+                        '(org-block-end-line   ((t (:extend t :overline nil :underline t :background "#FFFFDC"))))
+                        '(highlight   ((t (:inherit region :bold t :underline nil))))
+                        ))
 
     (when (niva/theme-is-active "ryerson")
       (custom-set-faces '(font-lock-comment-face ((t (:inherit 'unspecified :foreground "lightblue"))))
@@ -168,6 +176,13 @@
                         '(elfeed-search-unread-title-face ((t (:foreground "white"))))
                         '(font-lock-preprocessor-face     ((t (:foreground "green"))))))
 
+    (when (niva/theme-is-active "solarized")
+      (custom-set-faces '(font-lock-variable-name-face ((t (:weight unspecified)))))
+      (custom-set-faces '(font-lock-function-name-face ((t (:weight unspecified)))))
+      (custom-set-faces '(font-lock-constant-face      ((t (:weight unspecified)))))
+      (custom-set-faces '(font-lock-type-face          ((t (:weight unspecified)))))
+      (custom-set-faces '(font-lock-keyword-face       ((t (:weight unspecified))))))
+
     (when (or (niva/theme-is-active "wombat") (niva/theme-is-active "naysayer"))
       (custom-set-faces '(org-block            ((t (:background "#182C32"))))
                         '(org-block-begin-line ((t (:inherit 'default :extend t :overline t :underline nil :background "#182C32"))))
@@ -182,8 +197,22 @@
                         '(org-block-begin-line       ((t (:inherit 'org-block :extend t :overline t   :underline nil))))
                         '(org-block-end-line         ((t (:inherit 'org-block :extend t :overline nil :underline t))))))
 
+    (when (niva/theme-is-active "south")
+      (custom-set-faces
+       ;; '(elfeed-search-feed-face         ((t (:inherit 'fixed-pitch :bold nil :foreground unspecified))))
+       ;; '(elfeed-search-date-face         ((t (:inherit 'fixed-pitch))))
+       '(org-block                   ((t (:background nil))))
+       '(org-block-begin-line        ((t (:background nil :inherit 'org-block :extend t :overline t   :underline nil))))
+       '(org-block-end-line          ((t (:background nil :inherit 'org-block :extend t :overline nil :underline t))))))
+
+    ;; (when (niva/theme-is-active "orangey-bits")
+    ;;   (set-face-foreground 'default "#FFB240"))
+
+    ;; (when (niva/theme-is-active "vegetative")
+    ;;   (set-face-foreground 'default "#58B22C"))
+
     (custom-set-faces '(help-key-binding nil :box nil :background 'unspecified :foreground (face-attribute 'default :foreground)))
-    (custom-set-faces '(elfeed-search-unread-title-face ((t :inherit 'default :underline nil :bold t :foreground unspecified)))
+    (custom-set-faces '(elfeed-search-unread-title-face ((t :inherit 'default :underline nil :bold nil :foreground unspecified)))
                       '(elfeed-search-title-face        ((t (:inherit 'shadow :underline nil :foreground unspecified))))
                       '(elfeed-search-feed-face         ((t (:inherit 'default :underline nil :foreground unspecified))))
                       '(elfeed-search-tag-face          ((t (:inherit 'shadow :underline nil :foreground unspecified))))
@@ -197,8 +226,19 @@
   (setq nano-fonts-use nil
         nano-window-divider-show t))
 
+(custom-set-faces '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face)))))
+
 (with-eval-after-load 'eglot
-  (custom-set-faces '(eglot-highlight-symbol-face ((t (:underline nil :bold t))))))
+  (custom-set-faces
+   `(eglot-inlay-hint-face
+     ((t :inherit 'default
+         ;; :font "Arial"
+         :height 0.8
+         :italic nil
+         :inherit 'org-block-begin-line)))
+   '(eglot-highlight-symbol-face
+     ((t :underline nil
+         :bold t)))))
 
 (with-eval-after-load 'dired
   (custom-set-faces '(dired-directory ((t (:inherit link :underline nil))))))
